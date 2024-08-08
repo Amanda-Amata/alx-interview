69d3c6cf898fc00d6bd0391326b6e8fffbc53094
Amanda Mabunda <amandamabu14@gmail.com>
2024-08-08 20:32:42 +0000

    starwars film character list

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,16 +2,32 @@
 
 const request = require('request');
 
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
-  if (err) throw err;
-  const actors = JSON.parse(body).characters;
-  exactOrder(actors, 0);
-});
-const exactOrder = (actors, x) => {
-  if (x === actors.length) return;
-  request(actors[x], function (err, res, body) {
-    if (err) throw err;
-    console.log(JSON.parse(body).name);
-    exactOrder(actors, x + 1);
+const req = (arr, i) => {
+  if (i === arr.length) return; // Base case for recursion
+  request(arr[i], (err, response, body) => {
+    if (err) {
+      throw err; // Handle request error
+    } else {
+      const characterData = JSON.parse(body);
+      console.log(characterData.name); // Log character name
+      req(arr, i + 1); // Recursive call to process the next character
+    }
   });
 };
+
+// Main request to get film details
+request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, (err, response, body) => {
+  if (err) {
+    throw err; // Handle request error
+  } else {
+    const data = JSON.parse(body);
+    const chars = data.characters;
+
+    // Check if characters is defined and is an array
+    if (Array.isArray(chars) && chars.length > 0) {
+      req(chars, 0); // Call req with the characters array
+    } else {
+      console.log('No characters found for this film.');
+    }
+  }
+});
